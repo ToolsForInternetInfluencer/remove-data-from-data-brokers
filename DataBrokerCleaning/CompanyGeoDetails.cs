f3cfd721b2081caf5fd09726d7ffcf616781d2bd
ToolsForInternetInfluencer <129713063+ToolsForInternetInfluencer@users.noreply.github.com>
2024-01-13 20:27:47 -0700

    commit code

--- DataBrokerCleaning/CompanyGeoDetails.cs
+++ DataBrokerCleaning/CompanyGeoDetails.cs
@@ -1,12 +1,17 @@
-﻿using System;
+﻿using DataBrokerCleaning.Data;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TaskbarClock;
 
 namespace DataBrokerCleaning
 {
@@ -21,25 +26,66 @@ namespace DataBrokerCleaning
 
         private void CompanyGeoDetails_Load(object sender, EventArgs e)
         {
+
             //Company Time Zone
+            String CompanyTimeZone = "Company Time Zone";
             //Company Utc Offset
+            String CompanyUtcOffset = "CompanyUtcOffset";
             //Company Geo Street Number
+            String CompanyGeoStreetNumber = "Company Geo Street Number";
             //Company Geo Street Name
+            String CompanyGeoStreetName = "Company Geo Street Name";
             //Company Geo Sub Premise
+            String CompanyGeoSubPremise = "Company Geo Sub Premise";
             //Company Geo City
+            String CompanyGeoCity = "Company Geo City";
             //Company Geo Postal Code
+            String CompanyGeoPostalCode = "Company Geo Postal Code";
             //Company Geo State
+            String CompanyGeoState = "Company Geo State";
             //Company Geo State Code
+            String CompanyGeoStateCode = "Company Geo State Code";
             //Company Geo Country
+            String CompanyGeoCountry = "Company Geo Country";
             //Company Geo Country Code
+            String CompanyGeoCountryCode = "Company Geo Country Code";
             //Company Geo Lat
+            String CompanyGeoLat = "Company Geo Lat";
             //Company Geo Lng
+            String CompanyGeoLng = "Company Geo Lng";
 
 
 
 
 
+            string cs = ConfigurationManager.ConnectionStrings["DataBrokerCleaning.Properties.Settings.DataBrokerDatabaseConnectionString"].ConnectionString;
+            SqlConnection con = new SqlConnection(cs);
+            con.Open();
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "select * from DataBrokerDetails where Id = " + this.id;
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            CompanyGeoLocation geoLocation = new CompanyGeoLocation();
 
+
+            foreach (DataRow row in dt.Rows)
+            {
+                geoLocation.CompanyTimeZone = Convert.ToString(row[CompanyTimeZone]);
+                geoLocation.CompanyUtcOffset = Convert.ToString(row[CompanyUtcOffset]);
+                geoLocation.CompanyGeoStreetNumber = Convert.ToString(row[CompanyGeoStreetNumber]);
+                geoLocation.CompanyGeoStreetName = Convert.ToString(row[CompanyGeoStreetName]);
+                geoLocation.CompanyGeoSubPremise = Convert.ToString(row[CompanyGeoSubPremise]);
+                geoLocation.CompanyGeoCity = Convert.ToString(row[CompanyGeoCity]);
+                geoLocation.CompanyGeoPostalCode = Convert.ToString(row[CompanyGeoPostalCode]);
+                geoLocation.CompanyGeoState = Convert.ToString(row[CompanyGeoState]);
+                geoLocation.CompanyGeoStateCode = Convert.ToString(row[CompanyGeoStateCode]);
+                geoLocation.CompanyGeoCountry = Convert.ToString(row[CompanyGeoCountry]);
+                geoLocation.CompanyGeoCountryCode = Convert.ToString(row[CompanyGeoCountryCode]);
+                geoLocation.CompanyGeoLat = Convert.ToString(row[CompanyGeoLat]);
+                geoLocation.CompanyGeoLng = Convert.ToString(row[CompanyGeoLng]);
+            }
 
 
 
---
+++ DataBrokerCleaning/Data/CompanyGeoLocation.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBrokerCleaning.Data
+{
+   public  class CompanyGeoLocation
+    {
+        //Company Time Zone
+        public  String CompanyTimeZone {get; set;}
+        //Company Utc Offset
+        public String CompanyUtcOffset { get; set; }
+        //Company Geo Street Number
+        public String CompanyGeoStreetNumber { get; set; }
+        //Company Geo Street Name
+        public String CompanyGeoStreetName { get; set; }
+        //Company Geo Sub Premise
+        public String CompanyGeoSubPremise { get; set; }
+        //Company Geo City
+        public String CompanyGeoCity { get; set; }
+        //Company Geo Postal Code
+        public String CompanyGeoPostalCode { get; set; }
+        //Company Geo State
+        public String CompanyGeoState { get; set; }
+        //Company Geo State Code
+        public String CompanyGeoStateCode { get; set; }
+        //Company Geo Country
+        public String CompanyGeoCountry { get; set; }
+        //Company Geo Country Code
+        public String CompanyGeoCountryCode { get; set; }
+        //Company Geo Lat
+        public String CompanyGeoLat { get; set; }
+        //Company Geo Lng
+        public  String CompanyGeoLng { get; set; }
+
+
+
+    }
+}

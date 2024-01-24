9c674fdc427a9a3518bedc21108298dd643892a5
ToolsForInternetInfluencer <129713063+ToolsForInternetInfluencer@users.noreply.github.com>
2024-01-23 20:43:06 -0700

    added social

--- DataBrokerCleaning/CompanySocial.cs
+++ DataBrokerCleaning/CompanySocial.cs
@@ -1,9 +1,13 @@
-﻿using System;
+﻿using DataBrokerCleaning.Data;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,30 +16,47 @@ namespace DataBrokerCleaning
 {
     public partial class CompanySocial : Form
     {
-        public CompanySocial()
+        int id = 0;
+        public CompanySocial(int dataid)
         {
             InitializeComponent();
+            this.id = dataid;
         }
 
         private void CompanySocial_Load(object sender, EventArgs e)
-        {
-            /*
-             Company Facebook Handle
-Company Linkedin Handle
-Company Twitter Handle
-Company Twitter Id
-Company Twitter Bio
-Company Twitter Location
-Company Twitter Site
-Company Twitter Avatar
-Company Crunchbase Handle
+        { 
+
+            String CompanyFacebookHandle = "Company Facebook Handle";
+            String CompanyLinkedinHandle = "Company Linkedin Handle";
+            String CompanyTwitterHandle = "Company Twitter Handle";
+            String CompanyTwitterId = "Company Twitter Id";
+            String CompanyTwitterBio = "Company Twitter Bio";
+            String CompanyTwitterLocation = "Company Twitter Location";
+            String CompanyTwitterSite = "Company Twitter Site";
+            String CompanyCrunchbaseHandle = "Company Crunchbase Handle";
+            string cs = ConfigurationManager.ConnectionStrings["DataBrokerCleaning.Properties.Settings.DataBrokerDatabaseConnectionString"].ConnectionString;
+            SqlConnection con = new SqlConnection(cs);
+            con.Open();
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "select * from DataBrokerDetails where Id = " + this.id;
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+
+            CompanySocialData csd = new CompanySocialData();
+            foreach (DataRow row in dt.Rows)
+            {
+                csd.CompanyFacebookHandle = Convert.ToString(row[CompanyFacebookHandle]);
+                csd.CompanyLinkedinHandle = Convert.ToString(row[CompanyLinkedinHandle]);
+                csd.CompanyTwitterHandle = Convert.ToString(CompanyTwitterHandle);
+                csd.CompanyTwitterId = Convert.ToString(row[CompanyTwitterId]);
+                csd.CompanyTwitterBio = Convert.ToString(row[CompanyTwitterBio]);
+                csd.CompanyTwitterLocation = Convert.ToString(row[CompanyTwitterLocation]);
+                csd.CompanyTwitterSite = Convert.ToString(row[CompanyTwitterSite]);
+                csd.CompanyCrunchbaseHandle = Convert.ToString(row[CompanyCrunchbaseHandle]);
+            }
 
 
-
-
-
-
-             */
         }
-    }
 }
---
+++ DataBrokerCleaning/Data/CompanySocialData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBrokerCleaning.Data
+{
+    public class CompanySocialData
+    {
+        public String CompanyFacebookHandle { get; set; }
+        public String CompanyLinkedinHandle { get; set; }
+        public String CompanyTwitterHandle { get; set; }
+        public String CompanyTwitterId { get; set; }
+        public String CompanyTwitterBio { get; set; }
+        public String CompanyTwitterLocation { get; set; }
+        public String CompanyTwitterSite { get; set; }
+        public String CompanyCrunchbaseHandle { get; set; }
+    }
+}
